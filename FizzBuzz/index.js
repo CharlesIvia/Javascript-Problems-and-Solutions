07f49f1f6d97c85019d4c62841369183391c05e9
Charles Ivia <iviacharles@gmail.com>
2020-06-09 08:20:59 +0300

    Created project files and barebone html

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -2,7 +2,7 @@ let number = parseInt(prompt("Enter a number to find a FizzBuzz", 30));
 
 for (let i = 1; i <= number; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuz");
+    console.log("FizzBuzz");
   } else if (i % 3 === 0) {
     console.log("Fizz");
   } else if (i % 5 === 0) {
