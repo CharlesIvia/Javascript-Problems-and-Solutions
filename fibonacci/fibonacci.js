8b4f197cbf9324fb1df3c02b17300658bab1ea7f
Charles Ivia <iviacharles@gmail.com>
2020-06-22 13:33:33 +0300

    Fibonacci using a for loop

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,17 @@
+//Using a for loop
+
+const fibonacci = function (count) {
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+
+  return b;
+};
